228f2cbac12e7f91cfdfdaa3f318758645a1f218
NicolayBelyaev <nickb2147@gmail.com>
2023-04-28 09:23:37 +0300

    засабмитил конфиг в гит

---
+++ src/utils/confing.js
@@ -0,0 +1,7 @@
+export const config = {
+    baseURL: 'https://api.react-learning.ru',
+    headers: {
+        'Content-Type': "application/json",
+        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNlZDQ2NDMyOTFkNzkwYjNmMzRkZjQiLCJncm91cCI6Imdyb3VwLTEyIiwiaWF0IjoxNjgxODk5MTA3LCJleHAiOjE3MTM0MzUxMDd9.ax-as2BWqSsNMsR16nEnCGcihnqMx2yv5o6NIYaLAzw'
+    }
+}
