ec29650b2631fd23b5b46e838600b8a2e0bf1e86
NicolayBelyaev <nickb2147@gmail.com>
2023-06-15 15:26:22 +0300

    продолжаем делать корзину

--- src/pages/BasketPage/basketPage.jsx
+++ src/pages/BasketPage/basketPage.jsx
@@ -5,13 +5,33 @@ import s from './index.module.scss'
 
 export const BasketPage = () => {
     const cardsInBasket = useSelector(state => state.basket.basket)
+    const productsInBasket = cardsInBasket.reduce((accum, product) => accum + product.amountInBasket, 0)
+    const totalConstInBasket = cardsInBasket.reduce((accum, product) => accum + product.amountInBasket * product.price, 0)
 
-    return (
-                <div className={s.container}>
+    return (<>
+            {!!productsInBasket &&
+                <p className={s.container}>
+                    {productsInBasket} товаров в корзине.
+                </p>}
+
+            <div className={s.container}>
+                <div className={s.products}>
                     {cardsInBasket.map((item) => {
                         return <BasketCard key={item._id} {...item} product={item}/>
                     })}
-                    <hr></hr>
                 </div>
-            )
+                <div className={s.total}>
+                    <span>Ваша корзина</span>
+                    <div className={s.price}>
+                        <span>Товары ({productsInBasket})</span>
+                        <span>{totalConstInBasket}</span>
+                    </div>
+                    <div className={s.discount}>
+                        <span>Скидка</span>
+                        <span></span>
+                    </div>
+
+                </div>
+            </div>
+            </>)
 }
